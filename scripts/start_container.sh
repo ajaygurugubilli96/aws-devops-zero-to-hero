b73e710cb9a229a539d80b6b27ac09d4f78ef1d1
ajaygurugubilli96 <167063616+ajaygurugubilli96@users.noreply.github.com>
2024-09-13 14:19:01 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker push ajaygurugubilli/simple-python-flask-app
+docker pull ajaygurugubilli/simple-python-flask-app
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 docker push ajaygurugubilli/simple-python-flask-app 
